f4bde3fa2f400b44952f8bb747ff76607b65b79e
Vyacheslav <theelecman@gmail.com>
2017-04-04 11:25:42 +0300

    1

--- Shake/Shake/Program.cs
+++ Shake/Shake/Program.cs
@@ -10,8 +10,24 @@ namespace Shake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ебать ты крут");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            int x2 = 3;
+            int y2 = 5;
+            char sym2 = '%';
+
+            Draw(x1, y1, sym1);
+            Draw(x2, y2, sym2);
             Console.ReadLine();
+        }
+
+        // Функция которая отрисовывает точку
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
