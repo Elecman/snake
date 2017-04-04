5ea7466c6c09e1029d7d2518e6246f8633b9c381
Vyacheslav <theelecman@gmail.com>
2017-04-04 15:11:20 +0300

    Введение в ООП

---
+++ Shake/Shake/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shake
+{
+    class Point
+    {
+        //Создали класс Point в котором описываем те данные с которыми мы будем работать
+        public int x;
+        public int y;
+        public char sym;
+
+        //Описываем метод Draw, который будет отрисовывать точку для будущего обращения к нему
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
